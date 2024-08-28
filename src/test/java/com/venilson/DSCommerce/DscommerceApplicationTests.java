6288a18492a2b0c307015e3feeae1d71a0033b26
Venilson Neves <103954028+venilson0708@users.noreply.github.com>
2024-08-28 08:40:02 -0300

    projeto com entidades e relacionamentos criados

---
+++ src/test/java/com/venilson/DSCommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venilson.DSCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
